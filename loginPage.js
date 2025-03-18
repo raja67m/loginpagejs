664ee69e458c40ede0d14d3a15b9a3a7eb32139d
Raja <rajalingam210901@gmail.com>
2025-03-18 21:26:38 +0530

    Changed javascript code

--- loginPage.js
+++ loginPage.js
@@ -1,42 +1,36 @@
-const form=document.querySelector('.form');
+const form=document.getElementById('form');
 
 form.addEventListener('submit',function(event){
-  event.preventDefault();  // preventDefault function submission behaviour
+  event.preventDefault();  
+const userName=document.getElementById('userName').value;
+const passWord=document.getElementById('password').value;
+const userError=document.querySelector('.useralert');
+const passwordError=document.querySelector('.passwordalert');
+const emailPattren=/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const passwordPattren=/^[a-zA-Z0-9!@#\$%\^\&*_=+-]{8,}$/g;
+var isValid=true;
+
+if(!emailPattren.test(userName)){
+   userError.innerHTML="username don't match";
+   isValid=false;
+   return false;
+}
+else{
+  userError.innerHTML="";
+
+}
+if(!passwordPattren.test(passWord)){
+  passwordError.innerHTML="Password don't match";
+  isValid=false;
+  return false;
+}
+else{
+  passwordError.innerHTML="";
+  
+ 
+}
 
 
-  // geting the value of username & password
-
-  const userName=document.querySelector('.userName').value;
-  const password=document.querySelector('.password').value;
-
-  // checking the username & password
-
-  let isValid=true;
-
-      document.querySelector('.userName-alert').textContent="";
-      document.querySelector('.passWord-alert').textContent="";
-      
-      // check username
-      const username="raja67";
-      if(!username.match(userName)){
-         document.querySelector('.userName-alert').textContent="Please check your userName";
-         isValid=false;
-   }
-
-   // check password
-   const passWord="Ra$$$6";
-   if(!passWord.match(password)){
-      document.querySelector('.passWord-alert').textContent="Please Enter correct password";
-      isValid=false;
-   }
-
-
-
-//   if(userName==="raja67"&& password==="Ra@$$$a6"){
-//    alert("succfully loged in");
-//   }
-//   else{
-//    alert("Plese enter the correct userName or password");
-//   }
-
+return true;
+  
 });
